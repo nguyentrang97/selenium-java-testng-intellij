0185e6548b4286dadd8d03cf6b7bd236f311d4b4
Trang Nguyen <trangnguyen97.neu@gmail.com>
2023-10-17 13:57:29 +0700

    Commit

--- selenium/webdriver/Topic_20_Upload_File.java
+++ selenium/webdriver/Topic_20_Upload_File.java
@@ -48,6 +48,7 @@ public class Topic_20_Upload_File {
         driver.get("https://blueimp.github.io/jQuery-File-Upload/");
         By uploadFile = By.xpath("//input[@type='file']");
 
+        //Tải lên từng file 1
         driver.findElement(uploadFile).sendKeys(dalatPhotoPath);
         sleepInSecond(1);
 
@@ -82,7 +83,35 @@ public class Topic_20_Upload_File {
     }
 
     @Test
-    public void TC_02_() {
+    public void TC_02_Multy_File_Per_Time() {
+        driver.get("https://blueimp.github.io/jQuery-File-Upload/");
+        By uploadFile = By.xpath("//input[@type='file']");
+
+        //Tải nhiều file 1 lúc
+        driver.findElement(uploadFile).sendKeys(dalatPhotoPath + "\n" + hanoiPhotoPath + "\n" + quangninhPhotoPath);
+        sleepInSecond(2);
+
+        //Verify các file đc load lên thành công
+        Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + dalatPhoto + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + hanoiPhoto + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + quangninhPhoto + "']")).isDisplayed());
+
+        //Click upload cho từng file
+        List<WebElement> startButtons = driver.findElements(By.cssSelector("tbody.files button.start"));
+        for (WebElement start : startButtons) {
+            start.click();
+            sleepInSecond(2);
+        }
+
+        //Verify các file đuợc upload lên thành công
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + dalatPhoto + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + hanoiPhoto + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + quangninhPhoto + "']")).isDisplayed());
+
+        //Verify các hình này được upload lên là hình thực
+        Assert.assertTrue(isImageLoaded("//a[@title='" + dalatPhoto + "']/img"));
+        Assert.assertTrue(isImageLoaded("//a[@title='" + hanoiPhoto + "']/img"));
+        Assert.assertTrue(isImageLoaded("//a[@title='" + quangninhPhoto + "']/img"));
 
     }
 
